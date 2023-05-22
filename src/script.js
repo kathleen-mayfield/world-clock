87f1a4fd1c60d96775d604a128cfba81ed683463
kathleen-mayfield <kathleenkmayfield@gmail.com>
2023-05-22 14:47:29 -0600

    Added functionality of the <select>

--- src/script.js
+++ src/script.js
@@ -40,5 +40,25 @@ function updateTime() {
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[<small>]");
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city1">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss [<small>]A[<small>]")}
+      </div>
+    </div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
